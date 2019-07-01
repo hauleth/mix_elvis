0025c38627f27e0a54a41b3c6e060ac085f99d53
Łukasz Niemier <lukasz@niemier.pl>
2019-07-01 18:47:44 +0200

    chore: publish version 0.1.0

--- mix.exs
+++ mix.exs
@@ -6,11 +6,26 @@ defmodule MixElvis.MixProject do
       app: :mix_elvis,
       version: "0.1.0",
       elixir: "~> 1.6",
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
   defp deps do
-    [{:elvis, "~> 0.4.0", hex: :elvis_core}]
+    [
+      {:elvis, "~> 0.4.0", hex: :elvis_core},
+
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      description: "Run Elvis linter for project that are using Mix",
+      links: %{
+        "GitHub" => "https://github.com/hauleth/mix_elvis"
+      }
+    ]
   end
 end
